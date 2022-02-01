6aed3e09338a8ee99300c345661b44dcb3ca8c7e
Christopher Rose <christophervrose@gmail.com>
2022-02-01 12:19:04 +0530

    [refactor] dedup transaction data generation

--- components/DataFormatter/WholePdfGen.ts
+++ components/DataFormatter/WholePdfGen.ts
@@ -3,6 +3,7 @@ import jsPDF from 'jspdf';
 import autoTable from 'jspdf-autotable';
 import { parseISO } from "date-fns";
 import { format } from "date-fns";
+import { getTransactionRows, tableHeader } from './DataFormatting';
 
 /**
  * Generate a PDF
@@ -10,53 +11,10 @@ import { format } from "date-fns";
  */
 const generatePDF = ( transactions : Transaction[]) => {
 
-  const debitTranscations = transactions.filter(
-    (transaction) => transaction.type === "debit"
-  );
-  const creditTranscations = transactions.filter(
-    (transaction) => transaction.type === "credit"
-  );
-
-
-  const Balance =
-    transactions[0].amount +
-    creditTranscations.reduce((acc, curr) => acc + curr.amount, 0) -
-    debitTranscations.reduce((acc, curr) => acc + curr.amount, 0);
-
-  const parseRupees = (num: number) => {
-    return new Intl.NumberFormat("en-IN").format(num);
-  };
-
   const doc = new jsPDF();
-  const tableColumn = [
-    "Id",
-    "Date",
-    "society",
-    "Amount",
-    "Type",
-    "Balance",
-  ];
-  const tableRows = [];
-
-  var balance = 0;
-  transactions.forEach((data) => {
-    {
-      data.type === "credit" || data.type === "open"
-        ? (balance = balance + data.amount)
-        : (balance = balance - data.amount);
-    }
-    const ticketData = [
-      data.id,
-      data.date,
-      data.society,
-      parseRupees(data.amount),
-      data.type,
-      parseRupees(balance),
-    ];
-    return tableRows.push(ticketData);
-  });
-  const lastRow = ["Total Balance", "", "", "", "", parseRupees(Balance)];
-  tableRows.push(lastRow);
+  
+  const tableColumn = tableHeader;
+  const tableRows = getTransactionRows(transactions);
 
   autoTable(doc, {
     head: [tableColumn],
