56dd019b98cbf3f69a9c71779231a9ac59757e4b
Prajwal P <prajwalprakash3722@gmail.com>
2022-02-01 16:04:08 +0530

    [add] added basic react-table

--- components/BalanceTable.tsx
+++ components/BalanceTable.tsx
@@ -1,6 +1,8 @@
-/* eslint-disable @next/next/no-img-element */
 import Link from "next/link";
+import { useMemo } from "react";
+import { ColumnFilter } from "../Misc/ColFilter";
 import { Transaction } from "../types";
+import { useTable, usePagination, useFilters } from "react-table";
 
 type BalanceCardProps = {
   transaction: Transaction[];
@@ -35,62 +37,135 @@ const BalCard = ({ transaction }: BalanceCardProps) => {
     "Remarks",
   ];
 
+  const colFilterer = useMemo(() => {
+    return {
+      Filter: ColumnFilter,
+    };
+  }, []);
+
+  const data = useMemo(() => transaction, [transaction]);
+
+  const columns = useMemo(
+    () => [
+      {
+        Header: "Event",
+        accessor: "event",
+        width: 200,
+        filterable: true,
+        filter: colFilterer,
+      },
+      {
+        Header: "Date",
+        accessor: "date",
+        width: 200,
+        filterable: true,
+        filter: colFilterer,
+      },
+      {
+        Header: "Amount",
+        accessor: `amount`,
+        width: 200,
+        filterable: true,
+        filter: colFilterer,
+      },
+      {
+        Header: "Signed Off by",
+        accessor: "signedOff",
+        width: 200,
+        filterable: true,
+        filter: colFilterer,
+      },
+      {
+        Header: "Society",
+        accessor: "society",
+        width: 200,
+        filterable: true,
+        filter: colFilterer,
+      },
+      {
+        Header: "Type",
+        accessor: "type",
+        width: 200,
+        filterable: true,
+        filter: colFilterer,
+      },
+      {
+        Header: "Remarks",
+        accessor: "description",
+        width: 200,
+        filterable: true,
+        filter: colFilterer,
+      },
+    ],
+    []
+  );
+
+  function Table({ columns, data }: any) {
+    // Use the state and functions returned from useTable to build your UI
+    const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
+      useTable(
+        {
+          columns,
+          data,
+        },
+        useFilters
+      );
+
+    // Render the UI for your table
+    return (
+      <table
+        className="min-w-full divide-y divide-gray-800 "
+        {...getTableProps()}
+      >
+        <thead className="bg-gray-800">
+          {headerGroups.map((headerGroup, index) => (
+            <tr {...headerGroup.getHeaderGroupProps()} key={index}>
+              {headerGroup.headers.map((column) => (
+                <th
+                  className="px-6 py-3 text-left text-xs font-medium text-gray-300 uppercase tracking-wider"
+                  {...column.getHeaderProps()}
+                  key={index}
+                >
+                  {column.render("Header")}
+                </th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody
+          className="bg-gray-700 divide-y divide-gray-500"
+          {...getTableBodyProps()}
+        >
+          {rows.map((row, i) => {
+            prepareRow(row);
+            return (
+              <tr {...row.getRowProps()} key={i}>
+                {row.cells.map((cell) => {
+                  return (
+                    <td
+                      className="px-6 py-4 whitespace-nowrap text-sm font-medium text-white"
+                      {...cell.getCellProps()}
+                      key={i}
+                    >
+                      {cell.render("Cell")}
+                    </td>
+                  );
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    );
+  }
+
   return (
     <>
       <section className="flex flex-col  ">
         <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
           <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
             <div className="shadow overflow-hidden border-b border-gray-800 rounded-lg">
-              <table className="min-w-full divide-y divide-gray-800 ">
-                <thead className="bg-gray-800">
-                  <tr>
-                    {tableHeaders.map((header, index) => (
-                      <>
-                        <th
-                          scope="col"
-                          className="px-6 py-3 text-left text-xs font-medium text-gray-300 uppercase tracking-wider"
-                          key={index}
-                        >
-                          {header}
-                        </th>
-                      </>
-                    ))}
-                  </tr>
-                </thead>
-                <tbody className="bg-gray-700 divide-y divide-gray-500">
-                  {transaction.map((item) => (
-                    <tr key={item.id}>
-                      <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-white">
-                        {item.event}
-                      </td>
-                      <td className="px-6 py-4 whitespace-nowrap text-sm text-white">
-                        {item.date}
-                      </td>
-                      <td className="px-6 py-4 whitespace-nowrap text-sm text-white truncate">
-                        &#8377; {parseRupees(item.amount)}
-                      </td>
-                      <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-white">
-                        {item.signedOff}
-                      </td>
-                      <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-white">
-                        {item.society}
-                      </td>
-                      <td className="px-6 py-4 whitespace-nowrap capitalize">
-                        <span
-                          className={`flex-shrink-0 inline-block px-2 py-0.5 text-xs font-medium rounded-full ${TransactionType(
-                            item.type
-                          )}`}
-                        >
-                          {item.type}
-                        </span>
-                      </td>
-                      <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-white">
-                        {item.description}
-                      </td>
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
+              <Table columns={columns} data={data} />
             </div>
           </div>
         </div>
@@ -100,3 +175,21 @@ const BalCard = ({ transaction }: BalanceCardProps) => {
 };
 
 export default BalCard;
+
+// TODO Below extra things are to be added
+// ! The amount has not been parsed in the table.
+/*
+ {parseRupees(amount)}
+ 
+ */
+
+// ! This is the background color classifier for the table add this in type column
+{
+  /* <span
+  className={`flex-shrink-0 inline-block px-2 py-0.5 text-xs font-medium rounded-full ${TransactionType(
+    item.type
+  )}`}
+>
+  {item.type}
+</span>; */
+}
